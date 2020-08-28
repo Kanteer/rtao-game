033ff3065e9e562a8d2783b6247aaaf32b3cc853
kanteer <59344076+Kanteer@users.noreply.github.com>
2020-08-28 13:52:39 -0400

    Update localplayer1.lua

--- localplayer1.lua
+++ localplayer1.lua
@@ -1,2 +1,9 @@
---hi
---hi
+print("hi there fellow player")
+
+light.edit(instances).Fog = 0
+sleep(2)
+light.edit(instances).Fog = 50
+Part.ScreenGUI(TextLabel).type = true
+type("Loading...")
+what.load = true
+print("Finished Loading!")
